820eefbed2c0ffae0959c7f8f1962826c55f1168
vishnucipher <vishnuvardhankalva8@gmail.com>
2024-01-16 00:48:08 +0530

    all commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask ,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    render_template('index.html')
+def index_page():
+    return render_template('index.html')
 
-if __name__=="__main__"
+if __name__ == '__main__':
+    app.run(debug=True)
